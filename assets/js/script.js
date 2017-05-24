dace10b558a7136faa1643f0d7166b75112dbcb0
Paulo Alves <paulo1rm23@gmail.com>
2017-05-24 14:50:26 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+(function($, window) {
+	$('.parallax-window').parallax({imageSrc: 'assets/images/back.webp'});
+	$('.parallax-window2').parallax({imageSrc: 'assets/images/back2.webp'});
+})(jQuery, window);
---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+      <title>VT - Consultores & Contadores</title>
+
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"> 
+      
+      <link rel='stylesheet' href='assets/css/bootstrap.min.css' type='text/css' />
+      <link rel='stylesheet' href='assets/css/hover-min.css' type='text/css' />
+      <link rel='stylesheet' href='assets/css/style.css' type='text/css' />
+   </head>
+
+   <body>
+      <header>
+         <div class="nav">
+            <nav class="navbar navbar-default" id="meuMenu">
+               <div class="container">
+                  <div class="navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                     </button>
+                     <a class="navbar-brand hvr-wobble-top" href=""><img src="assets/images/logo.png" alt="Logo - i9x solutions" /></a>
+                  </div><!-- .navbar-header -->
+
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                     <ul class="nav navbar-nav">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#vt">A VT</a></li>
+                        <li><a href="#servicos">Serviços</a></li>
+                        <li><a href="#links">Links úteis</a></li>
+                        <li><a href="#Conhecimento">Conhecimento</a></li>
+                        <li><a href="#contato">Contato</a></li>
+                     </ul><!-- .nav -->
+                  </div><!-- .navbar-collapse -->
+               </div><!-- .container -->
+            </nav><!-- .navbar -->
+         </div><!-- .nav -->
+      </header>
+
+      <div class="parallax-window" data-parallax="scroll" data-image-src="assets/images/back.webp">
+         <div class="content">
+            <div>
+               <h1>Título Geral</h1>
+               <p>Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum.</p>
+            </div>
+         </div><!-- .content -->
+      </div><!-- .parallax-window -->
+
+      <section id="vt">
+         <div class="container">
+            <h1 class="tit-geral">A VT</h1>
+
+            <p class="p-geral">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+            <p class="p-geral">This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
+            <p class="p-geral">At Wix we’re passionate about making templates that allow you to build fabulous websites and it’s all thanks to the support and feedback from users like you! Keep up to date with New Releases and what’s Coming Soon in Wixellaneous in Support. Feel free to tell us what you think and give us feedback in the Wix Forum.</p>
+         </div><!-- .container -->
+      </section><!-- #vt -->
+
+      <section id="servicos">
+         <div class="container">
+            <h1 class="tit-geral">SERVIÇOS</h1>
+            
+            <div class="row">
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Assessoria Tributária</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Análises Gerenciais</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Abertura, Constituição e Alterações Empresariais</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Consultoria</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Auditoria e Perícia</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Contabilidade Eleitoral e Prestação de Contas</h3>
+                  <p class="p-geral">Nos últimos anos, as cobranças eleitorais tem aumentado e inserido cada vez mais a obrigatoriedade da prestação continua de serviços contábeis e do acompanhamento de um contador, não somente no período eleitoral, mas também no dia a dia das comissões municipais.</p>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Contabilidade Fiscal</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Contabilidade Gerencial</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Contabilidade Patrimonial</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Departamento Pessoal e RH</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Declarações</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Assessoria Tributária</h3>
+               </div><!-- .md-4 -->
+               <div class="col-md-4">
+                  <img src="assets/images/servicos.png" alt="Serviços - VT" />
+                  
+                  <h3 class="sub-geral">Elaboração de Demonstrativos</h3>
+               </div><!-- .md-4 -->
+            </div><!-- .row -->
+         </div><!-- .container -->
+      </section><!-- #servicos -->
+
+      <section id="links">
+         <div class="container">
+            <h1 class="tit-geral">LINKS ÚTEIS</h1>
+            
+            <div class="col-md-4">
+               <ul>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>                  
+               </ul>
+            </div><!-- .md-4 -->
+            <div class="col-md-4">
+               <ul>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+               </ul>
+            </div><!-- .md-4 -->
+            <div class="col-md-4">
+               <ul>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+                  <li>
+                     <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
+                  </li>
+               </ul>
+            </div><!-- .md-4 -->
+         </div><!-- .container -->
+      </section><!-- #links -->
+
+      <div class="parallax-window2" data-parallax="scroll" data-image-src="assets/images/back2.webp"></div>
+
+      <footer>
+         <div class="container center">
+            <div class="content">
+               <a class="hvr-wobble-top" href=""><img src="assets/images/logo.png" alt="Logo - i9x solutions" /></a>
+
+               <span>© 2017 I9X. Todos os direitos reservados.</span>
+            </div><!-- .content -->
+         </div><!-- .container .center -->
+      </footer>
+
+      <script type='text/javascript' src='assets/js/jquery.js'></script>
+      <script type='text/javascript' src='assets/js/bootstrap.min.js'></script>
+      <script src="https://use.fontawesome.com/6963733f46.js"></script>
+      <script type='text/javascript' src='assets/js/parallax.min.js'></script>
+      <script type='text/javascript' src='assets/js/script.js'></script>
+   </body>
+</html>
