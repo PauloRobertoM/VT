cc59797668ea981cb6700ba864771d0660ad7178
Paulo Alves <paulo1rm23@gmail.com>
2017-05-25 17:59:23 -0300

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 (function($, window) {
 	$('.parallax-window').parallax({imageSrc: 'assets/images/back.webp'});
 	$('.parallax-window2').parallax({imageSrc: 'assets/images/back2.webp'});
+
+	$('.nav a').on('click', function(){
+	    $('.btn-navbar').click(); //bootstrap 2.x
+	    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+	});
 })(jQuery, window);
--- index.php
+++ index.php
@@ -26,16 +26,16 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                      </button>
-                     <a class="navbar-brand hvr-wobble-top" href=""><img src="assets/images/logo.png" alt="Logo - i9x solutions" /></a>
+                     <a class="navbar-brand hvr-wobble-top" href=""><img class="logo" src="assets/images/logo.png" alt="Logo - VT Consultores & Contadores" /></a>
                   </div><!-- .navbar-header -->
 
                   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                      <ul class="nav navbar-nav">
-                        <li><a href="#home">Home</a></li>
+                        <li><a href="">Home</a></li>
                         <li><a href="#vt">A VT</a></li>
                         <li><a href="#servicos">Serviços</a></li>
                         <li><a href="#links">Links úteis</a></li>
-                        <li><a href="#Conhecimento">Conhecimento</a></li>
+                        <li><a href="#conhecimento">Conhecimento</a></li>
                         <li><a href="#contato">Contato</a></li>
                      </ul><!-- .nav -->
                   </div><!-- .navbar-collapse -->
@@ -68,68 +68,68 @@
             <h1 class="tit-geral">SERVIÇOS</h1>
             
             <div class="row">
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Assessoria Tributária</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Análises Gerenciais</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Abertura, Constituição e Alterações Empresariais</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Consultoria</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Auditoria e Perícia</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Contabilidade Eleitoral e Prestação de Contas</h3>
                   <p class="p-geral">Nos últimos anos, as cobranças eleitorais tem aumentado e inserido cada vez mais a obrigatoriedade da prestação continua de serviços contábeis e do acompanhamento de um contador, não somente no período eleitoral, mas também no dia a dia das comissões municipais.</p>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Contabilidade Fiscal</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Contabilidade Gerencial</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Contabilidade Patrimonial</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Departamento Pessoal e RH</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Declarações</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Assessoria Tributária</h3>
                </div><!-- .md-4 -->
-               <div class="col-md-4">
+               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                   <img src="assets/images/servicos.png" alt="Serviços - VT" />
                   
                   <h3 class="sub-geral">Elaboração de Demonstrativos</h3>
@@ -142,7 +142,7 @@
          <div class="container">
             <h1 class="tit-geral">LINKS ÚTEIS</h1>
             
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                <ul>
                   <li>
                      <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
@@ -155,7 +155,7 @@
                   </li>                  
                </ul>
             </div><!-- .md-4 -->
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                <ul>
                   <li>
                      <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
@@ -168,7 +168,7 @@
                   </li>
                </ul>
             </div><!-- .md-4 -->
-            <div class="col-md-4">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                <ul>
                   <li>
                      <a href="#" class="hvr-float-shadow" target="_blank"><div class="icon"></div><h3 class="sub-geral">Declarações</h3></a>
@@ -186,14 +186,60 @@
 
       <div class="parallax-window2" data-parallax="scroll" data-image-src="assets/images/back2.webp"></div>
 
-      <footer>
-         <div class="container center">
-            <div class="content">
-               <a class="hvr-wobble-top" href=""><img src="assets/images/logo.png" alt="Logo - i9x solutions" /></a>
+      <section id="conhecimento">
+         <div class="container">
+            <h1 class="tit-geral">CONHECIMENTO</h1>
 
-               <span>© 2017 I9X. Todos os direitos reservados.</span>
-            </div><!-- .content -->
-         </div><!-- .container .center -->
+            <p class="p-geral">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+            <p class="p-geral">This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
+            <p class="p-geral">At Wix we’re passionate about making templates that allow you to build fabulous websites and it’s all thanks to the support and feedback from users like you! Keep up to date with New Releases and what’s Coming Soon in Wixellaneous in Support. Feel free to tell us what you think and give us feedback in the Wix Forum.</p>
+         </div><!-- .container -->
+      </section><!-- #conhecimento -->
+
+      <section id="contato">
+         <div class="container">
+            <h1 class="tit-geral">CONTATO</h1>
+
+            <form action="https://formspree.io/paulo1rm23@gmail.com" method="POST">
+               <div class="row">
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                     <input type="text" name="Nome" value="" placeholder="NOME" aria-required="true" />
+                  </div><!-- .md-6 -->
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                     <input type="email" name="E-mail" value="" placeholder="E-MAIL" aria-required="true" />
+                  </div><!-- .md-6 -->
+
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                     <input type="tel" name="Telefone" value="" placeholder="TELEFONE" />
+                  </div><!-- .md-6 -->
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                     <input type="text" name="Assunto" value="" placeholder="ASSUNTO" aria-required="true" />
+                  </div><!-- .md-6 -->
+
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                     <textarea name="Mensagem" placeholder="MENSAGEM"></textarea>
+                  </div><!-- .md-12 -->
+               </div><!-- .row -->
+
+               <input type="submit" class="botao" value="Enviar">
+            </form>
+         </div><!-- .container -->
+      </section><!-- #contato -->
+
+      <footer>
+         <div id="rodape">
+            <div class="container">
+               <address>
+                  <span><i class="fa fa-mobile" aria-hidden="true"></i> 84 98888-8888</span>
+
+                  <div class="redes-sociais">
+                     <a href="" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                     <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                     <a href="" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                  </div><!-- .redes-sociais -->
+               </address>
+            </div><!-- .container -->
+         </div><!-- #rodape -->
       </footer>
 
       <script type='text/javascript' src='assets/js/jquery.js'></script>
